c05ce82982d6a0cf567ac5814ef8a67226931dfc
Tony Fideo <49733381+TonyFideo@users.noreply.github.com>
2026-09-18 17:53:31 -0300

    test(vita): compile filesystem before renderer boundary

--- src/sys/vita/vita_system.cpp
+++ src/sys/vita/vita_system.cpp
@@ -50,6 +50,7 @@ void Sys_Init( void ) {
 	sceIoMkdir( "ux0:data", 0777 );
 	sceIoMkdir( VITA_DATA_ROOT, 0777 );
 	sceIoMkdir( "ux0:data/Vita-OpenQ4/q4base", 0777 );
+	sceIoMkdir( "ux0:data/Vita-OpenQ4/baseoq4", 0777 );
 	sceIoMkdir( "ux0:data/Vita-OpenQ4/savegames", 0777 );
 	sceIoMkdir( "ux0:data/Vita-OpenQ4/config", 0777 );
 	sceIoMkdir( "ux0:data/Vita-OpenQ4/cache", 0777 );
